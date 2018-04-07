b8715f4a718161051e2923dc2e8c61f56368172e
Josh Litherland <josh@temp123.org>
2018-04-07 17:53:34 -0400

    Add serialhowdy

---
+++ serialhowdy.ino
@@ -0,0 +1,22 @@
+const int bufsize=32;
+char buf[bufsize];
+char outbuf[bufsize];
+int s;
+
+void setup() {
+  Serial.begin(115200,SERIAL_8N1);
+  Serial.println("ATL1");
+}
+
+void loop() {
+  Serial.println("Say something!\n");
+  s=Serial.readBytesUntil('\n',buf,bufsize);
+  if (s>0) {
+    // We got some bytes!
+    strncpy(outbuf,buf,s);
+    outbuf[s-1]='\0';
+    Serial.print("You said: ");
+    Serial.println(outbuf);
+  }
+  delay(100);
+}
